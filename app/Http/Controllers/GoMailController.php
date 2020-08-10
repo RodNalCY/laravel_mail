f2882e43b40eea01ce4a43cf52e66bcd5a0720d4
RodnalCode <rodnalcode@gmail.com>
2020-08-10 18:48:50 -0500

    Se creo el API de envio de correo

--- app/Http/Controllers/GoMailController.php
+++ app/Http/Controllers/GoMailController.php
@@ -14,7 +14,7 @@ class GoMailController extends Controller
 
     public function enviarEmail(Request $request){
 
-        if($request->ajax()) {
+      if($request->ajax()) {
 
             if($request['data']['email']){
                 //return response()->json($request['data']['email']);
@@ -33,6 +33,26 @@ class GoMailController extends Controller
             }else{
                 return response()->json(['resultado'=>'error']);
             }
+        }
+    }
+
+    public function enviarEmailApiModel(Request $request){
+
+        if($request['email']){
+            $mail_usuario = $request['email'];
+            $subject ="Envio de Email";
+            $for = $mail_usuario;
+            $data = array('name'=> 'Rodnal Cabello' , 'email_from' => 'sistemastiresoft@lab-elsol.com', 'name_from' => 'Julio Vela', 'url_link'=> "https://miro.medium.com/max/800/1*E3v7azePLUppA9_iELu1yQchrome.1.69i57j35i39j0l4j0i433j0i10.6554j1j7&sourceid=chrome&ie=UTF-8");
+
+            Mail::send('emails.email', $data, function($msj) use($subject, $for){
+                $msj->from('sistemastiresoft@lab-elsol.com',"SISTEMAS TELETRABAJO");
+                $msj->subject($subject);
+                $msj->to($for);
+            });
+
+            return response()->json(['resultado'=>'success']);
+        }else{
+            return response()->json(['resultado'=>'error']);
         }
     }
 }
